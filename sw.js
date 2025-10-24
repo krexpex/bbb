3045e7ce7116920b5e664de2b0c3af18aae6ea8b
Valentins <Filipovica@kiber.local>
2025-10-24 21:14:16 +0300

    add google verification tag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10089520
+    "zoneId": 10089534
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
